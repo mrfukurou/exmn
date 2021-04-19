1f1856eee2e28104f238b3d63efcb2c23a45c7d6
mrfukurou <63556549+mrfukurou@users.noreply.github.com>
2021-04-19 13:50:52 +0300

    3

--- src/ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
+++ src/ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
@@ -13,6 +13,48 @@ struct Subject {
 	int semester;
 };
 
+void sort(Subject* mas, int size)
+{
+
+	int i = 0;
+	int j = size - 1;
+	Subject mid;
+	mid.semester = mas[size / 2].semester;
+	do {
+		while (mas[i].semester < mid.semester) {
+			i++;
+		}
+		while (mas[j].semester > mid.semester) {
+			j--;
+		}
+		if (i <= j) {
+			Subject tmp = mas[i];
+			if (mas[i].semester == mas[j].semester) {
+				if (mas[i].name > mas[j].name)
+				{
+					tmp = mas[i];
+					mas[i] = mas[j];
+					mas[j] = tmp;
+				}
+			}
+			else {
+				Subject tmp = mas[i];
+				mas[i] = mas[j];
+				mas[j] = tmp;
+			}
+			i++;
+			j--;
+		}
+	} 
+	while (i <= j);
+	if (j > 0) {
+		sort(mas, j + 1);
+	}
+	if (i < size) {
+		sort(&mas[i], size - i);
+	}
+}
+
 
 void output(Subject* mas, int num)
 {
@@ -24,24 +66,31 @@ void output(Subject* mas, int num)
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	int num;
-	cout << "Введите количество элементов в массиве: ";
-	cin >> num;
+	try {
+		setlocale(LC_ALL, "Russian");
+		SetConsoleCP(1251);
+		SetConsoleOutputCP(1251);
+		int num;
+		cout << "Введите количество элементов в массиве: ";
+		cin >> num;
+		throw 1;
+		if (num < 0) {
+			Subject* Plan = new Subject[num];
 
-	Subject *Plan = new Subject[num];
-
-	for (int i = 0; i < num; i++) 
-	{
-		cout << "Введите название: ";
-		cin >> Plan[i].name;
-		cout << "Введите фамилию: ";
-		cin >> Plan[i].lastname;
-		cout << "Введите семестр: ";
-		cin >> Plan[i].semester;
-		cout << endl;
+			for (int i = 0; i < num; i++)
+			{
+				cout << "Введите название: ";
+				cin >> Plan[i].name;
+				cout << "Введите фамилию: ";
+				cin >> Plan[i].lastname;
+				cout << "Введите семестр: ";
+				cin >> Plan[i].semester;
+				cout << endl;
+			}
+			sort(Plan, num);
+			output(Plan, num);
+		}
 	}
-		output(Plan, num);
+	catch(int a)
+	{ if (a == 1) cout << "Введено некoрректное количество элементов" << endl; }
 }
