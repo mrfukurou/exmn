d16dc60375f3b2bbabcbf5eff0617d311c8b0bab
mrfukurou <63556549+mrfukurou@users.noreply.github.com>
2021-04-19 13:04:15 +0300

    2

--- src/ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
+++ src/ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
@@ -2,18 +2,46 @@
 //
 
 #include <iostream>
+#include <locale.h>
+#include <windows.h>
 
 using namespace std;
 
 struct Subject {
 	string name;
-	string Lastname;
+	string lastname;
 	int semester;
 };
 
-void sort(){}
+
+void output(Subject* mas, int num)
+{
+	for (int i = 0; i < num; i++)
+	{
+		cout << "Название: " << mas[i].name << "\nФамилия преподавателя: " << mas[i].lastname << "\nСеместр: " << mas[i].semester<<endl;
+	}
+}
 
 int main()
 {
-	
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int num;
+	cout << "Введите количество элементов в массиве: ";
+	cin >> num;
+
+	Subject *Plan = new Subject[num];
+
+	for (int i = 0; i < num; i++) 
+	{
+		cout << "Введите название: ";
+		cin >> Plan[i].name;
+		cout << "Введите фамилию: ";
+		cin >> Plan[i].lastname;
+		cout << "Введите семестр: ";
+		cin >> Plan[i].semester;
+		cout << endl;
+	}
+		output(Plan, num);
 }
