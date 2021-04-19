2b5afe3eaceb29d3092d7fd641dc352759f62600
mrfukurou <63556549+mrfukurou@users.noreply.github.com>
2021-04-19 12:29:33 +0300

    Add files via upload

---
+++ src/ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
@@ -0,0 +1,11 @@
+﻿// ConsoleApplication.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
